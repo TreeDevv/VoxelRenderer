846a158cf36d8bca0b2d43e2e9b537a22df63f5e
Hunter Paul <hpaulbus@gmail.com>
2025-10-28 15:10:16 -0500

    Perlin Terrain Generation (Basic)

--- src/World/ChunkGenerator.cpp
+++ src/World/ChunkGenerator.cpp
@@ -1,17 +1,22 @@
 #include "ChunkGenerator.h"
 
+#include "noise.hpp"
+#include <algorithm>
+
 void GameWorld::ChunkGenerator::generate(std::shared_ptr<Chunk> chunk)
 {
+    const static int seed = 4;
+    const static siv::PerlinNoise perlin(seed);
+
     for (int x = 0; x < 16; x++)
     {
-        for (int y = 0; y < 128; y++)
+        for (int z = 0; z < 16; z++)
         {
-            for (int z = 0; z < 16; z++)
-            {
-                if (y < x * 0.5f + 4)
-                {
-                    chunk->set(x, y, z, BlockID::DIRT);
-                }
+            auto chunkPos = chunk->getPos();
+            double noise = perlin.octave2D_01((x + chunkPos.x) * 0.01, (z + chunkPos.y) * 0.01, 3);
+            noise = floor(noise * 30);
+            for (int y = 0; y < noise; y ++) {
+                chunk->set(x, y, z, BlockID::DIRT);
             }
         }
     }
